4f14b93b2eca146877988497621bd0623bd807f0
suyogzz78 <sakurachhetri97@gmail.com>
2025-03-20 17:05:57 +0545

    matrix chain multiplication

--- LAB5/MATRIXCHAINMUL.c
+++ LAB5/MATRIXCHAINMUL.c
@@ -1,46 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <limits.h>
+#include <time.h>
+
 int matrixChainOrder(int p[], int n) {
-int m[n][n];
-for (int i = 1; i < n; i++)
-m[i][i] = 0;
-for (int L = 2; L < n; L++) { // L is chain length
-for (int i = 1; i < n - L + 1; i++) {
-int j = i + L - 1;
-m[i][j] = INT_MAX;
-for (int k = i; k <= j - 1; k++) {
-int q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
-if (q < m[i][j])
-m[i][j] = q;
-}
+    int m[n][n];
+    for (int i = 1; i < n; i++)
+        m[i][i] = 0;
+    
+    for (int L = 2; L < n; L++) {
+        for (int i = 1, j = i + L - 1; j < n; i++, j++) {
+            m[i][j] = INT_MAX;
+            for (int k = i; k < j; k++) {
+                int q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
+                if (q < m[i][j])
+                    m[i][j] = q;
+            }
+        }
+    }
+    return m[1][n - 1];
 }
 
-}
-return m[1][n - 1];
-}
 int main() {
-int n;
-printf("Enter the number of matrices: ");
-scanf("%d", &n);
-int* p = (int*)malloc((n + 1) * sizeof(int));
-if (p == NULL) {
-printf("Memory allocation failed\n");
-return 1;
-}
-printf("Enter the dimensions array (of size %d):\n", n + 1);
-for (int i = 0; i < n + 1; i++) {
-scanf("%d", &p[i]);
-}
-clock_t start, end;
-double cpu_time_used;
-start = clock();
-int minCost = matrixChainOrder(p, n + 1);
-end = clock();
-cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
-printf("Minimum number of multiplications is %d\n", minCost);
-printf("\nTime executed: %f seconds\n", cpu_time_used);
-free(p);
-return 0;
+    int n;
+    printf("Enter the number of matrices: ");
+    scanf("%d", &n);
+    
+    int *p = malloc((n + 1) * sizeof(int));
+    if (!p) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+    
+    printf("Enter the dimensions array: ");
+    for (int i = 0; i <= n; i++)
+        scanf("%d", &p[i]);
+    
+    clock_t start = clock();
+    int minCost = matrixChainOrder(p, n + 1);
+
+    
+    printf("Minimum multiplications: %d\n", minCost);
+    clock_t end = clock();
+    printf("Execution time: %f seconds\n", ((double)(end - start)) / CLOCKS_PER_SEC);
+    
+    free(p);
+    return 0;
 }
